db1f893d76bf22f2fbb55abc7feb0a489b732a29
Juliana Zhu <juliana.zhu@clark.de>
2020-06-01 14:17:50 +0200

    Add import for logs

--- etl.py
+++ etl.py
@@ -16,6 +16,7 @@ SONG_DATA_PATH = "data/song_data"
 LOG_DATA_PATH = "data/log_data"
 SONG_TABLE_COLS = ["song_id", "title", "artist_id", "year", "duration"]
 ARTIST_TABLE_COLS = ["artist_id", "artist_name", "artist_location", "artist_latitude", "artist_longitude"]
+TIME_TABLE_COLS = ["artist_id", "artist_name", "artist_location", "artist_latitude", "artist_longitude"]
 
 
 def get_files(filepath):
@@ -29,8 +30,9 @@ def get_files(filepath):
 
 
 def import_data(filepath):
+    """ Currently only gets first line """
     f = open(filepath, "r")
-    return f.read()
+    return f.readline()
 
 
 def validate_data(json_data):
@@ -60,9 +62,10 @@ def insert_data_to_table(cur, conn, df, insert_query):
     conn.commit()
 
 
-def import_song_data():
-    song_files = get_files(SONG_DATA_PATH)
-    json_data = import_data(song_files[0])
+def import_data_from_directory(dir_path):
+    """ Currently only gets first file """
+    files = get_files(dir_path)
+    json_data = import_data(files[0])
     validated_data = validate_data(json_data)
     return create_dataframe(validated_data)
 
@@ -74,9 +77,12 @@ def load_data_to_table(data_frame, insert_query, cols):
 
 
 def main():
-    song_data = import_song_data()
+    song_data = import_data_from_directory(SONG_DATA_PATH)
     load_data_to_table(song_data, song_table_insert, SONG_TABLE_COLS)
     load_data_to_table(song_data, artist_table_insert, ARTIST_TABLE_COLS)
+
+    log_data = import_data_from_directory(LOG_DATA_PATH)
+    return True
 
 
 if __name__ == "__main__":
